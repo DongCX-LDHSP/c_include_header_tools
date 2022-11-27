b98734c80edb65411aa72431e82f9de4a5f9d26e
SiangY <new.dongcx@gmail.com>
2022-11-27 21:18:25 +0800

    添加：配置文件，用于方便用户修改程序配置

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from typing import List
+
+
+# 工程的根目录
+BASE_DIR: str = os.path.realpath(os.path.abspath(os.path.dirname(__file__)))
+
+# 将工程根目录追加到系统环境变量中
+sys.path.append(BASE_DIR)
+
+# 读取文件时，支持的文件编码格式
+support_encodings: List[str] = [
+    'utf-8',
+    'gbk',
+]
+
+"""
+日志等级，从以下字符串中任选其一，从上到下，日志输出逐渐增多：
+- CRITICAL
+- ERROR
+- WARNING
+- INFO
+- DEBUG
+- NOTSET
+"""
+log_level: str = 'WARNING'
+# 日志文件
+log_file_path: str = os.path.join(BASE_DIR, 'me.log')
+# 日志文件打开默认，默认使用覆盖写模式，即：'w'
+log_file_open_mode: str = 'w'
